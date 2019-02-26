a6a6481beb36bab148162ed22f5dfd31cbc592db
riceCk <876496731@qq.com>
2019-02-26 11:34:30 +0800

    2019-2-26更新ip

--- server/config.js
+++ server/config.js
@@ -4,6 +4,9 @@ let conf = fs.readFileSync('./server.conf');
 
 let configArr = conf.toString().split('\r\n');
 for (let i = 0; i < configArr.length; i++) {
-  globalConfig[configArr[i].split('=')[0]] = configArr[i].split('=')[1];
+  // globalConfig[configArr[i].split('=')[0]] = configArr[i].split('=')[1];
+  let key = configArr[i].split('=')[0].trim()
+  let value = configArr[i].split('=')[1]
+  globalConfig[key] = value
 }
 module.exports = globalConfig;
