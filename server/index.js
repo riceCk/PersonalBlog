6c07180082f39a21fdc0f3b34bf7d19b5a324ac0
riceCk <876496731@qq.com>
2019-03-15 17:31:27 +0800

    2019-2-26更新ip

--- server/index.js
+++ server/index.js
@@ -3,9 +3,11 @@ let globalConfig = require('./config.js');
 let log = require('./log.js');
 let loader = require('./loader');
 let app = new express();
-const path = require('path')
+const path = require('path');
+const history = require('connect-history-api-fallback');
+// const connect = require('connect');
 
-// app.use(express.static('../' + globalConfig.page_path));
+app.use(history());
 app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 app.post('/editEveryDay', loader.get('/editEveryDay'));
@@ -20,6 +22,6 @@ app.get('/queryHotBlog', loader.get('/queryHotBlog'));
 app.get('/queryByTags', loader.get('/queryByTags'));
 
 app.listen(globalConfig.port, () => {
-  log('服务器已启动', 'server')
-  console.log('服务器已启动');
+    log('服务器已启动', 'server')
+    console.log('服务器已启动');
 })
